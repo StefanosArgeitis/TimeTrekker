0bb7c464623eee30995f769ca736b5016424c565
Sem1209 <semddevries@gmail.com>
2022-06-28 19:32:49 +0200

    timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    public float currentTime;
+
+    private float resetTime = 0f;
+
+    private void Update()
+    {
+        currentTime = currentTime += Time.deltaTime;
+        SetTimerText();
+
+        if (currentTime >= 3f)
+        {
+            Reset();
+        }
+
+    }
+
+    private void SetTimerText()
+    {
+        timerText.text = currentTime.ToString("0.00");
+    }
+
+    private void Reset()
+    {
+        currentTime = resetTime;
+        enabled = false;
+        SetTimerText();
+    }
+
+    private void Pause()
+    {
+        enabled = false;
+    }
+
+    private void Play()
+    {
+        enabled = true;
+    }
+}
